24734656f89cd85f5846ee1e59579c512b620ffe
Vivian <ndidiranjo@gmail.com>
2024-03-19 11:47:47 +0100

    adding tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,7 +6,9 @@ function App() {
 
   return (
     <>
-      
+      <div>
+        <p className='p-10 m-25 text333-red'>Munyia Ranjo needs a vacation</p> 
+      </div>
     </>
   )
 }
