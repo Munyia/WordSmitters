bf7b4f0b5bdcc26f0ed99b8323faa63340265f71
Vivian <ndidiranjo@gmail.com>
2024-03-21 10:58:22 +0100

    adding buttons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css'
+import Home from "./pages//Home";
+import Login from "./pages/Login";
 
 function App() {
 
   return (
     <>
-      <div>
-        
-
-      </div>
+     <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Home/>} />
+        <Route path="/login" element={<Login/>} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,10 +2,21 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div id='LoginBody'>
+    <div id='Login'>
         <div>
             <p>Are you a Reader or a Writer?</p>
             <p>Sign with us and let your Imagination, Ink and stories take flight</p>
+            <div>
+            <button class="button" data-text="Awesome">
+    <span class="actual-text">&nbsp;Reader&nbsp;</span>
+    <span aria-hidden="true" class="hover-text">&nbsp;Reader&nbsp;</span>
+</button>\
+<button class="button" data-text="Awesome">
+    <span class="actual-text">&nbsp;Writer&nbsp;</span>
+    <span aria-hidden="true" class="hover-text">&nbsp;Writer&nbsp;</span>
+</button>
+            </div>
+
             <form action="" class="container">
   <div class="input-container">
       <div class="input-content">
@@ -15,7 +26,7 @@ const Login = () => {
                   <input placeholder="Email" required="" type="text" class="input-is"/>  
                   <input placeholder="Password" required="" type="password" class="input-is"/>  
               </div>
-              <button>Subscribe</button>
+              <button id='but'>Subscribe</button>
           </div>
       </div>
   </div>
