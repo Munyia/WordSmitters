752c0dc14fa0dc079f08fc8bba2e052b56f3283f
Vivian <ndidiranjo@gmail.com>
2024-03-27 18:04:36 +0100

    making changes on header and landing page

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,5 +1,7 @@
 import bgimg from './res/bgimg.jpg'
+import bg0 from './res/bg0.png'
+import llogo from './res/llogo.png'
 
 
 
-export {bgimg}
+export {bg0, llogo, bgimg}
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import imageLogo from "../assets/res/imageLogo.jpg"
 import "./header.css"
 import { Link } from 'react-router-dom'
-import log from '../assets/res/log.png'
+import llogo from '../assets/res/log.png'
 
 const Header = () => {
   return (
-    <div className='text-white relative gap-5 pr flex w-full h-[10vh] bg-[rgb(198,82,0)] justify-between items-center' id='Navbar'>
+    <div className='text-white  bg-[#CD9564] relative gap-5 pr flex w-full h-[10vh] bg- justify-between items-center' id='Navbar'>
        <div className='flex justify-around text-center items-center gap-5 content-center'>
-        <img src={log} className='h-[8vh]' alt="" />
+        <img src={llogo} className='h-[16vh]' alt="" />
           <Link>About</Link>
+          <Link>Books</Link>
        </div>
        <div>
        <input type="text" autocomplete="off" name="text" class="input" placeholder="Search by Author, Title ..."/>
@@ -18,9 +19,10 @@ const Header = () => {
   <Link>Login</Link>
 <div class="group input-box gap-5 relative w-[10vw] flex justify-evenly pr-5 cursor-pointer">
   <Link>Sign Up</Link>
-  <div className="hidden text-white absolute top-[100%] bg-[#1c6355] rounded  group-hover:flex flex-col gap-2 p-2">
-            <Link to={''} className='hover:bg-slate-300 rounded-lg px-1'>Reader</Link>
-            <Link to={''} className='hover:bg-slate-300 rounded-lg px-1'>Writer</Link>
+  <div className="hidden text-black absolute top-[100%] bg-[#CD9564] rounded  group-hover:flex flex-col gap-2 p-2">
+            <Link to={''} className='hover:bg-[rgb(10,32,8)]  hover:text-white  rounded-lg px-1'>Reader</Link>
+            <Link to={''} className='hover:bg-[rgb(10,32,8)] hover:text-white rounded-lg px-1'>Writer</Link>
+            <div className=' border-b-[rgb(10,32,8)]'></div>
         </div>
        </div>
 </div>
