ffde253cfb5768da30d3d7c413207cb9033e898b
Vivian <ndidiranjo@gmail.com>
2024-04-02 14:49:13 +0100

    app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,6 @@ import llogo from './pages/Home';
 import Login from './pages/Login'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignUp from './pages/SignUp';
-import Footer2 from './components/Footer2';
 import Register from './pages/Register';
 
 function App() {
@@ -17,7 +16,6 @@ function App() {
       <Route path="/login" element ={<Login />}/>
       <Route path="/" element ={<Home/>}/>
       <Route path="/signup" element ={<SignUp/>}/>
-      <Route path="/footer" element ={<Footer2/>}/>
       <Route path="/register" element ={<Register/>}/>
     </Routes>
     </BrowserRouter>
