0cfe40cff3038ff293e50b460353a6813130eef4
Vivian <ndidiranjo@gmail.com>
2024-04-16 16:20:07 +0100

    bug fix

--- server/model/userModel.js
+++ server/model/userModel.js
@@ -48,6 +48,9 @@ const userSchema =mongoose.Schema(
     {
     timestamps: true
 })
+userSchema.methods.matchPassword = async function (enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password);
+}
 
 userSchema.pre("save", async function(next){
     const salt = await bcrypt.genSalt(10);
