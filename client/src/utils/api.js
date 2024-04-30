8e22eb1235688d8121163510ec2813f9f2bf999b
Vivian <ndidiranjo@gmail.com>
2024-04-30 14:13:24 +0100

    bug fix

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
-import dotenv from "dotenv"
-dotenv.config()
 
 const api = axios.create({
-    baseURL: process.env.MODE === "Devlopment" ?"http://localhost:5001/":"https://wordsmitters.onrender.com/",
+    baseURL:"https://wordsmitters.onrender.com/",
     withCredentials: true
     
 })
